68afdbe3a45a7f5a60a3428f370b57cddb1c0a5d
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-09-20 14:32:59 +0200

    [add]:generator random for range number

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
 const countDown = document.querySelector('h3');
+
+
+
 const limit = 5;
 countDown.innerHTML = limit;
+
+
+console.log(generateNumbers(1,10));
+
+
+
+
+
+// Functions
+
+// random 
+function generateNumbers(min,max){
+  const numbersRandom = [];
+  while(numbersRandom.length < 5){
+    const number = Math.floor(Math.random() * (max - min +1) + min)
+    if (!numbersRandom.includes(number)) {
+      numbersRandom.push(number);
+    }
+  }
+  return numbersRandom;
+}
