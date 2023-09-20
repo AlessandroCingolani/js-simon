5bb312c7897f7246014915b27b40df09617d6b13
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-09-20 14:52:47 +0200

    [add]:display random numbers, at timer  0  d-none

--- js/script.js
+++ js/script.js
@@ -1,18 +1,41 @@
 const countDown = document.querySelector('h3');
-
+const numbersRandom = document.querySelector('h2');
+ 
 
 
 const limit = 5;
+let counter = limit;
 countDown.innerHTML = limit;
 
 
-console.log(generateNumbers(1,10));
+start();
+
+numbersRandom.innerHTML = generateNumbers(1,30).join('-');
+
+
+
 
 
 
 
 
 // Functions
+
+
+// countdown
+function start(){
+  const clock = setInterval(function(){
+    counter--;
+    countDown.innerHTML = counter;
+    if(counter === 0){
+      clearInterval(clock)
+      countDown.classList.add('d-none')
+      numbersRandom.classList.add('d-none')
+    }
+  },1000)
+
+}
+
 
 // random 
 function generateNumbers(min,max){
